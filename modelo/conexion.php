d196202ab57d1e91076ed82706d44c10fac75667
rapicrash <baltamiranos02@gmail.com>
2025-09-17 10:00:20 -0500

    avanzando

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,6 +3,13 @@ class Conexion {
     public static function conectar(){
         $shot ="localhost";
         $usuario = "root";
-
+        $clave = "";
+        $bd = "db_libros";
+        $conexion= new mysqli($shot,$usuario;$clave,$bd);
+        if($conexion->connect_error){
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+        return $conexion;
     }
 }
+?>    
