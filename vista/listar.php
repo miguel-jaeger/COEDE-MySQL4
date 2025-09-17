f8548df49b71a15cd03d8e63b2e50622ed437b13
arlinalci-cloud <arlinalci@gmail.com>
2025-09-17 10:02:50 -0500

    se agrego los comandos en listar

--- vista/listar.php
+++ vista/listar.php
@@ -1,1 +1,28 @@
 <?php
+require_once "controlador/Controlador.php";
+$controlador = new Controlador();
+$resulado = $controlador->listar();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head><title>Listado de Libros</title></head>
+<body>
+<h1>Listado de Libros</h1>
+<a href="index.php?accion=registrar">Registrar Libro</a>
+<table border="1">
+<tr><th>ID</th><th>Título</th><th>Autor</th><th>Año</th><th>Acciones</th></tr>    
+<?php while($fila = $resultado->fetch_assoc()){ ?>
+<tr>
+<td><?php echo $fila['id']; ?></td>
+<td><?php echo $fila['titulo']; ?></td>
+<td><?php echo $fila['autor']; ?></td>
+<td><?php echo $fila['anoPublicacion']; ?></td>
+<td>
+<a href="index.php?accion=editar&id=<?php echo $fila['id']; ?>" >Editar</a>
+<a href="index.php?accion=eliminar&id=<?php echo $fila['id']; ?>" >Eliminar</a>
+</td>
+</tr>
+<?php } ?>
+</table>  
+</body>
+</html>
