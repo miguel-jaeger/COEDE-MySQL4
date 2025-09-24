b6925992b582bc6aaf073666759edadda1c8a45d
miguel-jaeger <miguel.jaeger@gmail.com>
2025-09-24 14:45:08 -0500

    update

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -8,8 +8,8 @@ class Controlador {
     public function listar(){
         return $this->modelo->listar();
     }
-    public function registrar($titulo,$autor,$ano,$resumen){
-        return $this->modelo->registrar($titulo,$autor,$ano,$resumen);
+    public function registrar($titulo,$autor,$ano){
+        return $this->modelo->registrar($titulo,$autor,$ano);
     }
     public function eliminar($id){
         return $this->modelo->eliminar($id);
